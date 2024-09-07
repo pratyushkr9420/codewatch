1ffa77d552a9db3f948a46dbb4970504492fd643
Pratyush Kumar <pkumar@MacBook-Air-20074.local>
2024-09-07 10:37:49 -0400

    setup prisma and added navbar active link feature

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,10 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { MdDashboard } from "react-icons/md";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+import { styles } from "./styles/themes";
 
 type Link = {
   name: string;
@@ -11,14 +15,23 @@ export default function NavBar() {
     { name: "Dashboard", href: "/" },
     { name: "Issues", href: "/issues" },
   ];
+  const currentPath = usePathname();
+  // `text-gray-400 hover:text-gray-700 text-xl`
   return (
     <nav className="flex space-x-8 h-10 items-center px-6 py-6 border-b-2">
       <Link href="/">
-        <MdDashboard size={20} />
+        <MdDashboard size={25} />
       </Link>
       <ul className="flex space-x-8">
         {navLinks.map((link, index) => (
-          <li key={index} className="text-gray-400 hover:text-gray-900 text-xl">
+          <li
+            key={index}
+            className={classNames({
+              [styles.text.regularText]: true,
+              [styles.text.hoverText]: true,
+              [styles.text.activeText]: link.href === currentPath,
+            })}
+          >
             <Link href={link.href}>{link.name}</Link>
           </li>
         ))}
---
+++ app/styles/themes.ts
@@ -0,0 +1,7 @@
+export const styles = {
+  text: {
+    regularText: "text-gray-400 text-xl",
+    hoverText: "hover:text-gray-700",
+    activeText: "text-gray-900 text-bold",
+  },
+};
