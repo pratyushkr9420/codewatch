543e288fc1aad8e0f59efc34dcaeaa6435994f7f
Pratyush Kumar <pkumar@MacBook-Air-20074.local>
2024-09-15 09:51:37 -0400

    fixed access to session details on nav

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -7,7 +7,14 @@ import classNames from "classnames";
 import { styles } from "./styles/themes";
 import { NavLink } from "./types/types";
 import { useSession } from "next-auth/react";
-import { Box, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Button,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 export default function NavBar() {
   const navLinks: NavLink[] = [
@@ -17,8 +24,8 @@ export default function NavBar() {
   const currentPath = usePathname();
   const { data: session, status } = useSession();
   return (
-    <nav className="h-14 px-6 py-6 border-b-2 mb-5">
-      <Flex direction="row" justify="between">
+    <nav className="h-14 px-6 py-3 border-b-2 mb-5">
+      <Flex direction="row" justify="between" align="center">
         <Flex gap="2" align="center">
           <Link href="/">
             <MdDashboard size={25} />
@@ -40,7 +47,27 @@ export default function NavBar() {
         </Flex>
         <Box>
           {status === "authenticated" && (
-            <Link href="/api/auth/signout">Logout</Link>
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger>
+                <Avatar
+                  src={
+                    session.user && session.user.image ? session.user.image : ""
+                  }
+                  fallback="?"
+                  size="2"
+                  radius="full"
+                  className="cursor-pointer"
+                />
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content>
+                <DropdownMenu.Label>
+                  <Text>{session.user!.email}</Text>
+                </DropdownMenu.Label>
+                <DropdownMenu.Item>
+                  <Link href="/api/auth/signout">Logout</Link>
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
           )}
           {status === "unauthenticated" && (
             <Link href="/api/auth/signin">Login</Link>
