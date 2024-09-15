d5c99d272d30845b9061ed4e38cfae96c3fc582e
Pratyush Kumar <pkumar@MacBook-Air-20074.local>
2024-09-14 21:28:19 -0400

    setup prisma adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,21 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: "jwt", // Need to sepcify strategy for the session to make it jwt again
+  },
 });
 
 export { handler as GET, handler as POST };
+
+// Notes: Adding the prisma adapter switches strategy to database which does not work with Google OAuth
